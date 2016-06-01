170f39bcf4146b6b4939d3086f2a9451db469468
Domas Trijonis <domas.trijonis@maersk.com>
2016-06-01 15:07:38 +0200

    Updated from master

--- dist/md-range-slider.js
+++ dist/md-range-slider.js
@@ -6,30 +6,42 @@
     if(!$scope.step){
       $scope.step = 1;
     }
-    $scope.lowerMax = $scope.max - $scope.step;
-    $scope.upperMin = $scope.lowerValue + $scope.step;
-    if(!$scope.lowerValue || $scope.lowerValue<$scope.min){
-      $scope.lowerValue = $scope.min;
-    }else{
-      $scope.lowerValue*=1;
+    $scope.$watchGroup(['min','max'],minMaxWatcher);
+    $scope.$watch('lowerValue',lowerValueWatcher);
+    
+    function minMaxWatcher() {
+      $scope.lowerMax = $scope.max - $scope.step;
+      $scope.upperMin = $scope.lowerValue + $scope.step;
+      
+      if(!$scope.lowerValue || $scope.lowerValue < $scope.min){
+        $scope.lowerValue = $scope.min;
+      }else{
+        $scope.lowerValue*=1;
+      }
+      if(!$scope.upperValue || $scope.upperValue > $scope.max){
+        $scope.upperValue = $scope.max;
+      }else{
+        $scope.upperValue*=1;
+      }
+      updateWidth();
     }
-    if(!$scope.upperValue || $scope.upperValue>$scope.max){
-      $scope.upperValue = $scope.min;
-    }else{
-      $scope.upperValue*=1;
-    }
-
-    $scope.$watch('lowerValue',function(){
+    
+    function lowerValueWatcher() {
       if($scope.lowerValue >= $scope.upperValue - $scope.step){
         $scope.lowerValue = $scope.upperValue - $scope.step;
         return;
       }
       $scope.upperMin = $scope.lowerValue + $scope.step;
-      $scope.upperWidth = ((($scope.max-($scope.lowerValue + $scope.step))/($scope.max - $scope.min)) * 100) + "%";
+      
+      updateWidth();
+    }
+    
+    function updateWidth() {
+      $scope.upperWidth = ((($scope.max-($scope.lowerValue + $scope.step))/($scope.max-$scope.min)) * 100) + "%";
       if($scope.lowerValue > ($scope.upperValue - $scope.minGap) && $scope.upperValue < $scope.max) {
         $scope.upperValue = $scope.lowerValue + $scope.minGap;
       }
-    });
+    }
   }]);
 
   module.directive('rangeSlider', function () {
@@ -45,10 +57,10 @@
       template: [
         '<div class="range-slider-container">',
           '<div class="range-slider-left">',
-            '<md-slider aria-label="upperValue" ng-model="lowerValue" step="{{step}}" min="{{min}}" max="{{lowerMax}}"></md-slider>',
+            '<md-slider aria-label="upperValue" step="{{step}}" ng-model="lowerValue" min="{{min}}" max="{{lowerMax}}"></md-slider>',
           '</div>',
           '<div class="range-slider-right" ng-style="{width: upperWidth}">',
-            '<md-slider aria-label="upperValue" ng-model="upperValue" step="{{step}}" min="{{upperMin}}" max="{{max}}"></md-slider>',
+            '<md-slider aria-label="upperValue" step="{{step}}" ng-model="upperValue" min="{{upperMin}}" max="{{max}}"></md-slider>',
           '</div>',
         '</div>'
       ].join(''),
